95d9b7ed3c52ebc060892f9947f8f39e9e33dcfc
chiiico1303 <fsalesbezerra@gmail.com>
2023-11-13 14:27:22 -0300

    initial commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    Id: '1',
+    title: 'NOVO HOMEM DE FERRO ANUNCIADO',
+    description: 'Marvel studios anuncia o novo homem de ferro',
+    photoCover:
+      'https://gamerview.uai.com.br/wp-content/uploads/2020/06/marvels-iron-man-vr.jpg',
+  },
+  {
+    Id: '2',
+    title: 'Nova serie saiu no Disney+',
+    description: 'ola',
+    photoCover:
+      'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+  {
+    Id: '3',
+    title: 'Marvel vem com novas series e filmes pra 2024',
+    description: 'test',
+    photoCover:
+      'https://www.tribunapr.com.br/wp-content/uploads/sites/56/2022/01/20103920/marvel-studios.jpg',
+  },
+  {
+    Id: '4',
+    title: 'Disney Studios e netflix anunciam parceria de streaming',
+    description: 'ola',
+    photoCover: 'https://t2.tudocdn.net/598045?w=1920&h=1440',
+  },
+];
